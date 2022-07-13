ca351b399c04a58572cd7fc98ae8cb38d691e616
asahwells <asahwells@gmail.com>
2022-07-13 14:10:50 +0100

    feat: new

--- src/component/SearchBar.tsx
+++ src/component/SearchBar.tsx
@@ -1,7 +1,7 @@
 import {View, TextInput, Button, Image} from 'react-native';
 import React, {FC} from 'react';
 import tw from 'twrnc';
-// import useAppointment from '../utils/useRestaurant';
+// import UseRestaurant from '../utils/useRestaurant';
 // import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 // import Ionicons from 'react-native-vector-icons/Entypo';
 // import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -13,15 +13,16 @@ interface IProps {
 const SearchBar: FC<IProps> = ({search, setSearch}) => {
   return (
     <View
-      style={tw`mt-4 flex-row p-2 bg-[#eee] rounded-lg border-red-500 justify-between items-center`}>
+      style={tw`mt-4 flex-row p-2 bg-[#eee] rounded-lg border-red-500 justify-between`}>
       <TextInput
         value={search}
         onChangeText={setSearch}
         placeholder="Search here"
       />
+
       <Image
         source={require('../assets/images/search.png')}
-        style={tw`w-4 h-4`}
+        style={tw`w-4 h-4 p-2`}
       />
     </View>
   );
